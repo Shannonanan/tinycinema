298c6b8b60044077c29f445b5940f36f55b28dd3
Shannon Ferguson <shannonf238@gmail.com>
2018-08-18 19:02:50 +0200

    adapter get movies

---
+++ app/src/main/java/co/za/tinycinema/Constants.java
@@ -0,0 +1,7 @@
+package co.za.tinycinema;
+
+public final class Constants {
+    private Constants() {}
+
+    public static final String BASE_URL = "https://api.nasa.gov";
+}
--- app/src/main/java/co/za/tinycinema/features/GetMoviesInTheatres/MoviesInTheatresAdapter.java
+++ app/src/main/java/co/za/tinycinema/features/GetMoviesInTheatres/MoviesInTheatresAdapter.java
@@ -1,4 +1,78 @@
 package co.za.tinycinema.features.GetMoviesInTheatres;
 
-public class MoviesInTheatresAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import com.bumptech.glide.Glide;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import co.za.tinycinema.R;
+import co.za.tinycinema.features.GetMoviesInTheatres.domain.model.Result;
+
+public class MoviesInTheatresAdapter extends RecyclerView.Adapter<MoviesInTheatresAdapter.GetMoviesViewHolder>{
+
+    List<Result> getMoviesCollection;
+    private Context mContext;
+
+    public MoviesInTheatresAdapter(Context context) {
+        this.getMoviesCollection = Collections.emptyList();
+        mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public GetMoviesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int layoutIdForListItem = R.layout.cell_movies;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, parent, shouldAttachToParentImmediately);
+        GetMoviesViewHolder viewHolder = new GetMoviesViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GetMoviesViewHolder holder, int position) {
+        Result movieResult = this.getMoviesCollection.get(position);
+
+        String movieCode = movieResult.getPosterPath();
+        String url = mContext.getString(R.string.image_base_url) + movieCode;
+        Glide.with(mContext)
+                .load(url)
+                .into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return (this.getMoviesCollection != null) ? this.getMoviesCollection.size() : 0 ;
+    }
+
+    static class GetMoviesViewHolder extends RecyclerView.ViewHolder{
+            @BindView(R.id.cell_movie)ImageView imageView;
+        public GetMoviesViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+    public void setInfoCollection(Collection<Result> INFO) {
+        this.validateCollection(INFO);
+        this.getMoviesCollection = (List<Result>) INFO;
+        this.notifyDataSetChanged();
+    }
+
+    private void validateCollection(Collection<Result> infoCollection) {
+        if (infoCollection == null) {
+            throw new IllegalArgumentException("The list cannot be null");
+        }
+    }
 }
